1fff5f9241fcb6b35b6da170bb43b2e2ee2564e6
Elvira Fuente <hebebicho@gmail.com>
2019-07-02 10:21:18 +0200

    fix package.json

---
+++ docs/precache-manifest.4c6f7b801d37e9a59c431beb84317ce8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914a2ac4509f53984de6544aa4ebda95",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e58170774a5aabeae68b",
+    "url": "./static/css/main.fefa4059.chunk.css"
+  },
+  {
+    "revision": "16075e0b48a798d24536",
+    "url": "./static/js/2.26b02e89.chunk.js"
+  },
+  {
+    "revision": "e58170774a5aabeae68b",
+    "url": "./static/js/main.6469090f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
